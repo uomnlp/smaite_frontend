2f413d04565a090f6a057ff5cda69c22ac3a0cc5
Aditya Agarwal <aditya@student.manchester.ac.uk>
2022-07-16 18:44:36 +0100

    Configure deployment address

--- src/config.js
+++ src/config.js
@@ -8,7 +8,7 @@ const config = {
     },
     prod:{
         server:{
-            host: ''
+            host: 'http://localhost:8000'
         },
         fact_api_path : 'https://factchecktools.googleapis.com/v1alpha1/claims:search',
         fact_api_key: "AIzaSyCrhF3JQ_rbEpv1j9rXhUjNypjlrBbmJ2Y"
