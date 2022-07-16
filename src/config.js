49a31a8516428a0e326eb647cc7e113cab953526
Aditya Agarwal <aditya@student.manchester.ac.uk>
2022-07-16 23:06:23 +0100

    Configure production backend url

--- src/config.js
+++ src/config.js
@@ -8,7 +8,7 @@ const config = {
     },
     prod:{
         server:{
-            host: 'http://localhost:8000'
+            host: 'https://www.smaite.tech:8000'
         },
         fact_api_path : 'https://factchecktools.googleapis.com/v1alpha1/claims:search',
         fact_api_key: "AIzaSyCrhF3JQ_rbEpv1j9rXhUjNypjlrBbmJ2Y"
